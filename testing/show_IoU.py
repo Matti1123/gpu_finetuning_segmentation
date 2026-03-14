8266568b9258fd47a16606d0b4974611918bd8aa
Matti1123 <fm9023@mci4me.at>
2026-03-14 21:24:04 +0100

    showiuo

--- testing/show_IoU.py
+++ testing/show_IoU.py
@@ -1,5 +1,5 @@
 import torch
 
-ckpt = torch.load("runs/exp_first/best.pt", map_location="cpu")
+ckpt = torch.load("runs/deeplab_v3_plus/best.pt", map_location="cpu")
 print("Beste Val IoU:", ckpt["best_val_iou"])
 print("Epoche:", ckpt["epoch"])
